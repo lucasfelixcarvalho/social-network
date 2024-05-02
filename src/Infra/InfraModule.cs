bdafdd0dd079702f5ebfe24485d6511ad17b8dcf
Lucas Felix Carvalho <lucas.felixcarvalho@hotmail.com>
2024-05-02 19:09:06 -0300

    Basic project structure

---
+++ src/Infra/InfraModule.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Infra;
+
+public static class InfraModule
+{
+    public static IServiceCollection AddInfra(this IServiceCollection services)
+    {
+
+        return services;
+    }
+}
---
+++ src/Persistence/PersistenceModule.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Persistence;
+
+public static class PersistenceModule
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection services)
+    {
+
+        return services;
+    }
+}
